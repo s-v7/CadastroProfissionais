e1bbc98edfc7cd1e7227fb3b7420b185f969a717
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2024-05-08 09:30:09 -0300

    Update ConfiguracaoDeSeguranca.java

--- src/main/java/com/crea/cadastro/Models/ConfiguracaoDeSeguranca.java
+++ src/main/java/com/crea/cadastro/Models/ConfiguracaoDeSeguranca.java
@@ -1,3 +1,16 @@
+package com.crea.cadastro.Models;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableWebSecurity
