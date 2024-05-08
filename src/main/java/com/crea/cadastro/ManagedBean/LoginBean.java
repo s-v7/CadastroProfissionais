24e73f323d31b638464eeda18f8df566ee0ff302
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2024-05-08 09:35:47 -0300

    Update LoginBean.java

--- src/main/java/com/crea/cadastro/ManagedBean/LoginBean.java
+++ src/main/java/com/crea/cadastro/ManagedBean/LoginBean.java
@@ -1,11 +1,11 @@
-
+package com.crea.cadastro.ManagedBean;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named
 public class LoginBean {
-
+    /* Poderia chamar a class CredenciaisAutentications */ 
     private String username;
     private String password;
 
