0556f4b5326e3a1ac7d53e07e235c5d783a43524
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2024-05-08 09:12:13 -0300

    Update CadastroProfissionaisController.java
    Remove Lines

--- src/main/java/com/crea/cadastro/Controllers/CadastroProfissionaisController.java
+++ src/main/java/com/crea/cadastro/Controllers/CadastroProfissionaisController.java
@@ -2,6 +2,7 @@
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +22,7 @@ public class CadastroProfissionaisController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
-    
+   
     @ApiOperation(value = "Buscar todos os Profissionais Cadastrados", notes = "Retorna uma lista de todos os profissionais cadastrados no sistema.")
     @GetMapping
     public ResponseEntity<List<CadastroProfissionais>> getAllProfissionais() {
@@ -30,64 +30,61 @@ public class CadastroProfissionaisController {
         return ResponseEntity.ok().body(profissionaisAll);
     }
 
-    
     @ApiOperation(value = "Buscar um Profissional Cadastrado", notes = "Retorna um profissional com base no ID fornecido.")
     @GetMapping("/{id}")
     public ResponseEntity<CadastroProfissionais> getProfissionalById(@ApiParam(value = "ID do Profissional", required = true) @PathVariable int id) {
         CadastroProfissionais profissional = cadastroProfissionaisService.getProfissionalById(id);
         if (profissional == null) {
-            return ResponseEntity.notFound().build();
+		return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok().body(profissional);
     }
-
    
     @ApiOperation(value = "Cadastrar Novo Profissional", notes = "Cadastra um novo profissional no sistema.")
     @PostMapping
     public ResponseEntity<CadastroProfissionais> createProfissional(@ApiParam(value = "Novo Profissional", required = true) @RequestBody CadastroProfissionais newProfissional) 
         if (newProfissional == null) {
-            	return ResponseEntity.badRequest().build();
+		return ResponseEntity.badRequest().build();
         }
         CadastroProfissionais createdProfissional = cadastroProfissionaisService.postProfissional(newProfissional);
         return ResponseEntity.status (HttpStatus.CREATED).body(createdProfissional);
     }
 
-    /* Método Atualizar um Profissional Cadastrado*/
     @ApiOperation(value = "Atualizar um Profissional Cadastrado", notes = "Atualiza as informações de um profissional existente.")@PutMapping("/{id}")
-        public ResponseEntity<CadastroProfissionais> updateProfissional(@ApiParam(value = "ID do Profissional", required = true)@PathVariable int id,
+    public ResponseEntity<CadastroProfissionais> updateProfissional(@ApiParam(value = "ID do Profissional", required = true)@PathVariable int id,
                                                                 @ApiParam(value = "Profissional Atualizado", required = true)
                                                                 @RequestBody CadastroProfissionais updatedProfissional) {
-            CadastroProfissionais existeCadastro = cadastroProfissionaisService.getProfissionalById(id);
-            if(existeCadastro == null) {
-                return ResponseEntity.notFound().build();
-            }
-            // Verifica se há mudanças no CPF e senha antes de criptografar novamente
-            if(!existeCadastro.getCpf().equals(updatedProfissional.getCpf()) {
-                updatedProfissional.setCpf(passwordEncoder.encode(updatedProfissional.getCpf()));
-            }
-            if (!existeCadastro.getPassword().equals(updatedProfissional.getPassword())) {
-                updatedProfissional.setPassword(passwordEncoder.encode(updatedProfissional.getPassword()));
-            }
-            /* Atualizar campos necessário*/
-            existeCadastro.setTipoCadastro(updatedProfissional.getTipoCadastro());
-            existeCadastro.setNome(updatedProfissional.getNome());
-            existeCadastro.setEmail(updatedProfissional.getEmail());
-            existeCadastro.setDataNascimento(updatedProfissional.getDataNascimento());
-            existeCadastro.setTelefone(updatedProfissional.getTelefone());
-            existeCadastro.setTitulos(updatedProfissional.getTitulos());	
-            CadastroProfissionais updatedProfissional = cadastroProfissionaisService.putProfissional(existeCadastro);
-            return ResponseEntity.ok().body(updatedProfissional);	
+        CadastroProfissionais existeCadastro = cadastroProfissionaisService.getProfissionalById(id);
+        if(existeCadastro == null) {
+        	return ResponseEntity.notFound().build();
+        }
+	// Verifica se há mudanças no CPF e senha antes de criptografar novamente
+	if(!existeCadastro.getCpf().equals(updatedProfissional.getCpf()) {
+		updatedProfissional.setCpf(passwordEncoder.encode(updatedProfissional.getCpf()));
 	}
+	if (!existeCadastro.getPassword().equals(updatedProfissional.getPassword())) {
+		updatedProfissional.setPassword(passwordEncoder.encode(updatedProfissional.getPassword()));
+	}
+	/* Atualizar campos necessário*/
+	existeCadastro.setTipoCadastro(updatedProfissional.getTipoCadastro());
+	existeCadastro.setNome(updatedProfissional.getNome());
+        existeCadastro.setEmail(updatedProfissional.getEmail());
+        existeCadastro.setDataNascimento(updatedProfissional.getDataNascimento());
+        existeCadastro.setTelefone(updatedProfissional.getTelefone());
+        existeCadastro.setTitulos(updatedProfissional.getTitulos());	
+        CadastroProfissionais updatedProfissional = cadastroProfissionaisService.putProfissional(existeCadastro);
+        return ResponseEntity.ok().body(updatedProfissional);	
+    }
 
-	/* Método Deletar um Profissional Cadastrado*/
-	@ApiOperation(value = "Deletar um Profissional Cadastrado", notes = "Exclui um profissional do sistema com base no ID fornecido.")
-        @DeleteMapping("{id}")
-        public ResponseEntity<?> deleteCadastroProfissional(@ApiParam(value = "ID do Profissional", required = true)@PathVariable int id){
-            CadastroProfissionais existeCadastro = cadastroProfissionaisService.getProfissionalById(id);		
-            if(existeCadastro == null) {
-                return ResponseEntity.notFound().build();
-            }
-            cadastroProfissionaisService.deleteProfissional(id);
-            return ResponseEntity.ok().build();
-	}
+	
+     @ApiOperation(value = "Deletar um Profissional Cadastrado", notes = "Exclui um profissional do sistema com base no ID fornecido.")
+     @DeleteMapping("{id}")
+     public ResponseEntity<?> deleteCadastroProfissional(@ApiParam(value = "ID do Profissional", required = true)@PathVariable int id){
+	CadastroProfissionais existeCadastro = cadastroProfissionaisService.getProfissionalById(id);		
+        if(existeCadastro == null) {
+		return ResponseEntity.notFound().build();
+        }
+        cadastroProfissionaisService.deleteProfissional(id);
+        return ResponseEntity.ok().build();
+      }
 }
