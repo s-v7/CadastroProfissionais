c88cb4c18a1f5a570e154a72831ef5a904d777e4
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2024-05-08 09:36:57 -0300

    Update CadastroProfissionaisRepository.java
    Add imports

--- src/main/java/com/crea/cadastro/Repositorys/CadastroProfissionaisRepository.java
+++ src/main/java/com/crea/cadastro/Repositorys/CadastroProfissionaisRepository.java
@@ -1,9 +1,8 @@
-
+package com.crea.cadastro.Repositorys;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Reposiroty
 public interface CadastroProfissionaisRepository extends JpaRepository<CadastroProfissionais, int> {
-    // Métodos personalizados, se necessário
 
 }
