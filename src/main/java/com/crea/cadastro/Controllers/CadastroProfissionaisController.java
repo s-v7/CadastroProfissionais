b70d7eefe8aec4772088a117aac5369729268e4f
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2024-05-08 09:20:36 -0300

    Update CadastroProfissionaisController.java

--- src/main/java/com/crea/cadastro/Controllers/CadastroProfissionaisController.java
+++ src/main/java/com/crea/cadastro/Controllers/CadastroProfissionaisController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import com.crea.cadastro.Services.CadastroProfissionaisService;
+import com.crea.cadastro.Models.CadastroProfissionais;
 
 @RestController
 @RequestMapping("/api/cadastro")
